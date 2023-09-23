e15d4b8e84870192a6abfea453932dfa920530c5
ehsan <ehsan.shahrestani1@gmail.com>
2023-09-23 19:54:42 +0330

    new change

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -4,12 +4,17 @@ import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo:'aboutMe',
+    pathMatch:'prefix'
+  },
+  {
     path: '', component: LayoutComponent,
     children: [
-      {path:'resume', loadComponent:() => import('./pages/resume/resume.component').then(c => c.ResumeComponent)},
-      {path:'blog', loadComponent:() => import('./pages/blog/blog.component').then(c => c.BlogComponent)},
-      {path:'contact', loadComponent:() => import('./pages/contact/contact.component').then(c => c.ContactComponent)},
-      {path:'aboutMe', loadComponent:() => import('./pages/aboutme/aboutme.component').then(c => c.AboutmeComponent)},
+      { path: 'resume', loadComponent: () => import('./pages/resume/resume.component').then(c => c.ResumeComponent) },
+      { path: 'blog', loadComponent: () => import('./pages/blog/blog.component').then(c => c.BlogComponent) },
+      { path: 'contact', loadComponent: () => import('./pages/contact/contact.component').then(c => c.ContactComponent) },
+      { path: 'aboutMe', loadComponent: () => import('./pages/aboutme/aboutme.component').then(c => c.AboutmeComponent) },
     ]
 
   },
